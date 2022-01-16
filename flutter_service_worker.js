e32fa2a5a205fbb52c87bb41c26702b77af20072
yamaneco28 <yamaneco28@users.noreply.github.com>
2022-01-16 06:44:32 +0000

    deploy: RingTeam/ring_neo@54d6c1be44205f1ccfb2a34dca9c75e8ac0dc432

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "version.json": "9dc93da86da6ce4748609f077471519e",
-"main.dart.js": "1b79077966db485de627bbee2ec4d042",
+"main.dart.js": "c97740860bff36bac49b73fc17c9509c",
 "manifest.json": "52ceaf1a06581308206d3322dfb87a03",
 "assets/FontManifest.json": "1f51acc04e4851844466921643394958",
 "assets/assets/selectTagPage/%25E3%2583%2586%25E3%2582%25AF%25E3%2583%258E%25E3%2583%25AD%25E3%2582%25B8%25E3%2583%25BC.png": "b17eff8d889f50105ac4c4fc69980a86",
@@ -61,8 +61,8 @@ const RESOURCES = {
 "assets/fonts/CorporateLogo/logotypejp_mp_b_1.1.ttf": "ffac0fbc30fbbfa74d32b4affe2ff16c",
 "assets/fonts/CorporateLogo/logotypejp_mp_m_1.1.ttf": "d6a77ff8dae5f42a140c937fc48970c2",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"index.html": "84c2ab213e8e850065c70aff93c4c4f9",
-"/": "84c2ab213e8e850065c70aff93c4c4f9"
+"index.html": "cc123087e46fe0231418835ea837baf3",
+"/": "cc123087e46fe0231418835ea837baf3"
 };
 
 // The application shell files that are downloaded before a service worker can
